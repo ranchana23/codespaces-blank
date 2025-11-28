dd2cc3a311aa515e36e978b4920cdaa916fe2349
ranchana23 <ranchana@netkasystem.com>
2025-11-28 14:23:25 +0700

    auto crop

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the root directory
 app.use(express.static(path.join(__dirname, '.')));
 
+// Handle client-side routes by serving index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
